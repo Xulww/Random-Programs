6095533b1d67dd3ea24eda0fd0ef1a901b070c21
Jordan <49492517+Xulww@users.noreply.github.com>
2020-07-09 15:43:23 +0300

    Create animal.py

---
+++ Terrarium-Simulation/animal.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self, kind, status, hunger, location):
+        self.kind = kind
+        self.status = status
+        self.hunger = hunger
+        self.location = location
