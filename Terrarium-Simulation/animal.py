771fc6878ced59b86d125c4105992c969516145a
Jordan <49492517+Xulww@users.noreply.github.com>
2020-07-15 15:46:20 +0300

    Delete animal.py

--- Terrarium-Simulation/animal.py
+++
@@ -1,6 +0,0 @@
-class Animal:
-    def __init__(self, kind, status, hunger, location):
-        self.kind = kind
-        self.status = status
-        self.hunger = hunger
-        self.location = location
