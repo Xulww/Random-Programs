2dd85d89447f0957ff53090eed851be4678e92ca
Jordan <49492517+Xulww@users.noreply.github.com>
2020-11-28 18:32:55 +0200

    Update ApiKeys.js

--- ConscientiousDrivers/assets/ApiKeys.js
+++ ConscientiousDrivers/assets/ApiKeys.js
@@ -1,12 +1,12 @@
 export default {
   FirebaseConfig: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: "",
+    apiKey: "AIzaSyAoJfB3hwF6MeZZpmgVzLFdlWHDtmQuOzg",
+    authDomain: "mobileappsuni.firebaseapp.com",
+    databaseURL: "https://mobileappsuni.firebaseio.com",
+    projectId: "mobileappsuni",
+    storageBucket: "mobileappsuni.appspot.com",
+    messagingSenderId: "644271321377",
+    appId: "1:644271321377:web:c84da279b75131f3410e60",
+    measurementId: "G-ZYE6R0H9N6",
   },
 };
