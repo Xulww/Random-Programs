83852154b371ef8bff66740df11b84a18777e037
Jordan <49492517+Xulww@users.noreply.github.com>
2020-11-28 18:22:04 +0200

    Add files via upload

---
+++ ConscientiousDrivers/assets/ApiKeys.js
@@ -0,0 +1,12 @@
+export default {
+  FirebaseConfig: {
+    apiKey: "AIzaSyAoJfB3hwF6MeZZpmgVzLFdlWHDtmQuOzg",
+    authDomain: "mobileappsuni.firebaseapp.com",
+    databaseURL: "https://mobileappsuni.firebaseio.com",
+    projectId: "mobileappsuni",
+    storageBucket: "mobileappsuni.appspot.com",
+    messagingSenderId: "644271321377",
+    appId: "1:644271321377:web:c84da279b75131f3410e60",
+    measurementId: "G-ZYE6R0H9N6",
+  },
+};
